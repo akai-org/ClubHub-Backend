4ac4f7bed0fd380a6d7f24928e91bc9a5473f503
Seweryn <seweryn.wasilewski@gmail.com>
2024-07-02 11:49:35 +0200

    error handler fix

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,60 +1,78 @@
-const { default: AppError } = require('../utils/appError');
+const AppError = require('../utils/appError');
 
 require('dotenv').config()
 
-const prodError = (err, res) =>{
-    if (err.isOperational) {
-        res.status(err.statusCode).json({
-          status: err.status,
-          message: err.message,
-        });
-      } else {
-        res.status(500).json({
-          status: "error",
-          message: "something went very wrong!",
-        });
-      }
+const prodError = (err, appErr, res) =>{
+  console.log("prod Error")
+    if (appErr && appErr.isOperational) {
+      res.status(appErr.statusCode).json({
+        success : false, 
+        ...appErr, 
+      });
+    } else {
+      res.status(500).json({
+        success : false, 
+        status: "error",
+        message: "Internal server error",
+      });
+    }
 }
 
-const devError = (err, res) =>{
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack,
-      });
+const devError = (err, appErr, res) =>{
+  console.log("dev Error")
+  res.status(err.statusCode).json({
+      success : false, 
+      status: err.status,
+      ...appErr,
+      //more info about error in development mode
+      message: err.message,
+      stack: err.stack,
+      error : err,
+  });
 }
 
 const castErrorHandler = (err) =>{
     const message = `Invalid ${err.path}: ${err.value}.`
-    return new AppError(message, 400)
+    return new AppError(message, 400,{errType : "cast"})
 }
 
 const duplicateErrorHandler = (err) =>{
-    const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
-    const message = `field value:${value} aleady exist. please use another`;
-    return new AppError(message, 409)
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `field value:${value} aleady exist. please use another`;
+  return new AppError(message, 409, { errType : 'duplication', duplicatedFields : [value]})
 }
 
-const validateErroHandler = (err) =>{
+const validationErrorHandler = (err) =>{
     const errors = Object.values(err.errors).map((el) => el.message);
     const message = `Invalid input data. ${errors.join(". ")}`;
-    return new AppError (message, 400);
+    return new AppError(message, 400, {errType : "validation"});
+}
+
+const logErrors = (err, req, res, next) =>{
+  console.error(err)
+  next(err)
 }
 
 const errorHandler = (err, req, res, next) =>{
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-    
-  if (process.env.NODE_ENV === "development") {
-    devError(err, res);
-  } else if (process.env.NODE_ENV === "production") {
-    if (err.name === "CastError") err = castErrorHandler(err);
-    if (err. name === "ValidationError") err = validationErrorHandler (err); //error when not all fields are provided but are required
-    if (err.code === 11000 || err.code === 11001) err = duplicateErrorHandler(err) //error code is mongodb specific check mongodb documentation for more info
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
 
-    prodError(err, res);
+  let appErr; 
+
+  if (err.name === "CastError") appErr = castErrorHandler(err);
+  if (err. name === "ValidationError") appErr = validationErrorHandler (err); //error when not all fields are provided but are required
+  if (err.code === 11000 || err.code === 11001) appErr = duplicateErrorHandler(err); //error code is mongodb specific check mongodb documentation for more info
+  if (err instanceof AppError) appErr = err; 
+
+
+  if (process.env.NODE_ENV === "development") {
+    devError(err,appErr, res);
+  }
+  if (process.env.NODE_ENV === "production") {
+    prodError(err,appErr, res);
   }
 }
+
+
 
 module.exports = {errorHandler,logErrors}
--- backend/utils/appError.js
+++ backend/utils/appError.js
@@ -1,9 +1,10 @@
-export default class AppError extends Error {
-    constructor(message, statusCode) {
+module.exports =  class AppError extends Error {
+    constructor(message, statusCode, info) {
       super(message);
       this.statusCode = statusCode;
       this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
       this.isOperational = true;
+      Object.assign(this, info);
       Error.captureStackTrace(this, this.constructor);
     }
   }
